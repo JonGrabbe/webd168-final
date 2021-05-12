371a618b3ef8340e3d688341719275b56b714125
Jon <jongrabbe@outlook.com>
2021-05-12 15:44:02 -0700

    added menu button toggle

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,14 @@
-document.getElementById('open-menu-button').addEventListener('click', () => {
+document.getElementById('menu-button').addEventListener('click', function(e) {
     //opens header menu
     document.getElementById('menu').classList.toggle('menu-opened');
+    //change button image
+    let isOpen = document.getElementById('menu').classList.contains('menu-opened');
+    console.log(isOpen);
+    const buttonImg = '<img src="images/close.svg"/>';
+    const openMenu = '<img src="images/menu.svg"/>"';
+    if(isOpen) {
+        e.currentTarget.innerHTML = buttonImg;
+    } else {
+        e.currentTarget.innerHTML = openMenu;
+    }
 })
